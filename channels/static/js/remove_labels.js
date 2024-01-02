d3588db0c0e3f97b061edb08232da8e73fb29312
mehdirussel <35711089+mehdirussel@users.noreply.github.com>
2024-01-02 14:30:56 +0100

    regrouped staticfiles in one dir and removed duplicates, merged login and signup css into one

--- channels/static/js/remove_labels.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let labels = document.querySelectorAll('label');
-    labels.forEach(function(label) { label.setAttribute('hidden', true); });
-    });
