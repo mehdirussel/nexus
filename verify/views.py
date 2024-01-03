990cf49ee173c94497049275cd618f85ad7959c8
mehdirussel <35711089+mehdirussel@users.noreply.github.com>
2024-01-03 23:19:52 +0100

    mm

--- verify/views.py
+++ verify/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.core.signing import TimestampSigner,BadSignature,SignatureExpired
 from base64 import urlsafe_b64decode
 from users.models import NexusUser
-from users.views import send_email,gen_email_token
+from users.views import send_verif_email
 
 email_verification_delay = 2*24*3600 # 2 days (in seconds)
 DEST_AFTER_VALIDATION = 'login.html'
@@ -26,7 +26,8 @@ def validate_token(request,token_to_check):
         user_with_expired_link = TimestampSigner().unsign_object(urlsafe_b64decode(token_to_check).decode(),max_age=360*24*3600) # 360 days,  big enough to always be verified
         user_expired = get_object_or_404(NexusUser,email=user_with_expired_link["email"])
         if not user_expired.is_active: # if user hasnt already activated his account, otherwise we shouldnt resend a new link
-            messages.error(request, f'This verification link is expired, click <a href="{send_email(request,user_with_expired_link["email"])}">here</a> to get a new one')
+            send_verif_email(request,user_with_expired_link["email"])
+            messages.error(request, 'This verification link is expired, a new one has been sent to your email address.')
         else:
             messages.error(request, 'Your account is already verified!')
         return render(request, DEST_IF_FAILED)
